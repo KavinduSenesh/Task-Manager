10470108ec37d49e483a7577d7c3347575f27c44
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-10 17:27:59 +0530

    Add task list and form components with service integration

---
+++ task-manager-frontend/src/app/services/task.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:8080/api/v1/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getAllTasks(): Observable<any>{
+    return this.http.get(`${this.apiUrl}/getAll`);
+  }
+
+  deleteTask(id: number): Observable<any>{
+    return this.http.delete(`${this.apiUrl}/delete/${id}`);
+  }
+}
