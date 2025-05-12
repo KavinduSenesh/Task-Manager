306e3a32979161337a76896dcdfac0e4baccc187
Kavindu Senesh <ksenesh5@gmail.com>
2025-05-12 20:32:23 +0530

    Format TaskDTO.java by adding a new line at the end of the file

--- task-manager-backend/task-manager/src/main/java/lk/ijse/task_manager/dto/TaskDTO.java
+++ task-manager-backend/task-manager/src/main/java/lk/ijse/task_manager/dto/TaskDTO.java
@@ -14,5 +14,6 @@ public class TaskDTO {
     private String title;
     private String description;
     private String status;
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt; 
+    
 }
